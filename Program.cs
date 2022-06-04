d0a99b0b8e9d2417bc5133d0c72a4237c8eee8e5
Alexander2119m <105405177+Alexander2119m@users.noreply.github.com>
2022-06-03 22:24:03 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Examen1erparcialAdonis
+{
+    class Program
+    {
+        public static void Main(string [] args)
+        {
+            EstudiantePrimaria primaria = new EstudiantePrimaria();
+            primaria.Datos("Raul ","Cevallos Vera","Fe alegria #2 ",131675482);
+            primaria.ImprimirDatos();
+
+            EstudianteSecundaria Secundaria = new EstudianteSecundaria();
+            Secundaria.Datos("Pedro Anthonio","Intriago chavez","Pedro Fermin ",1316452874);
+            Secundaria.DatosExtra(3);
+            Secundaria.ImprimirDatos();
+
+            EstudianteUniversitario Universitario = new EstudianteUniversitario();
+            Universitario.Datos("Carlos Alberto","Cedeño Intriago","ULEAM",1316465827);
+            Universitario.DatosEx(5);
+            Universitario.ImprimirDatos();
+        }
+    }
+}
